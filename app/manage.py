6987460457682890544fc6db3c10805a3e7084fd
Gil <sgy@tpay.co.kr>
2019-11-12 20:37:49 +0900

    [Lintfix]

--- app/manage.py
+++ app/manage.py
@@ -5,9 +5,11 @@ import sys
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
     try:
-        from django.core.management import execute_from_command_line
+        from django.core.management import (  # pylint: disable=C0415
+            execute_from_command_line,
+        )
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
--- app/users/apps.py
+++ app/users/apps.py
@@ -5,9 +5,3 @@ class UsersAppConfig(AppConfig):
 
     name = "users"
     verbose_name = "Users"
-
-    def ready(self):
-        try:
-            import users.signals  # pylint: disable=W0611
-        except ImportError:
-            pass
